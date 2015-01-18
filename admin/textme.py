1b69f059acfcb25cc1395bf0599425502a565c1d
Thomas Propst <tom.propst@gmail.com>
2015-01-18 10:14:56 -0700

    Added options to textme.py
    Now you can use -c to specify the configuration file and you must use -m to specify the message (breaking change - maybe I should change this). There is improved error handling too.  The configuration file will make it easier to use this in larger systems and on different platforms.

--- admin/textme.py
+++ admin/textme.py
@@ -2,7 +2,10 @@
 #****************************************************************************
 #* file: textme.py
 #* author: Tom Propst
-#* Sends the message passed as the sole argument as an SMS message.
+#* Sends the message passed after -m as an SMS to the number specified
+#* in the configuration file.
+#* By default, ~/twilio.conf is used as the configuration file unless
+#* another file is specified after the -c option.
 #****************************************************************************
 
 import sys
@@ -11,12 +14,16 @@ import urllib
 import urllib2
 import xml.etree.ElementTree as ET
 import socket
+import getopt
+from os.path import expanduser
 
-hostName = socket.gethostname()
-if len(sys.argv) > 1:
-    msgBody = hostName + ": " + sys.argv[1]
-else:
-    msgBody = hostName + ": <empty message>"
+home = expanduser('~')
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'c:m:')
+except getopt.GetoptError as err:
+    print str(err)
+    sys.exit(2)
 
 # Twilio stuff
 # You must have a file listed below that contains 4 lines for:
@@ -24,13 +31,38 @@ else:
 # Twilio Authentication Token
 # Twilio Phone Number
 # Recipient Phone Number
-twilioConfigFile = '/home/pi/twilio.conf'
-params = [line.strip() for line in open(twilioConfigFile)]
+twilioConfigFile = home + '/twilio.conf'
+msgBody = '<empty message>'
+
+for o, a in opts:
+    if o == '-c':
+        twilioConfigFile = a
+    elif o == '-m':
+        msgBody = a
+    else:
+        assert False, 'unhandled exception'
+
+hostName = socket.gethostname()
+msgBody = hostName + ': ' + msgBody
+
+# if len(sys.argv) > 1:
+#     msgBody = hostName + ": " + sys.argv[1]
+# else:
+#     msgBody = hostName + ": <empty message>"
+
+try:
+    params = [line.strip() for line in open(twilioConfigFile)]
+except:
+    raise
+
 if len(params) >= 4:
     accountSid = params[0]
     authToken = params[1]
     fromNum = params[2]
     toNum = params[3]
+else:
+    print 'Invalid configuration file ' + twilioConfigFile
+    sys.exit(2)
 
 baseUrl = 'https://api.twilio.com/2010-04-01/Accounts/' + \
     accountSid +  '/SMS/Messages'
